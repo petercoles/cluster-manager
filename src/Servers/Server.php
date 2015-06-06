26ea1dae41c4bf07e04c083412ca129980f2b04b
Peter Coles <peter@kuroiweb.com>
2015-06-06 06:00:51 +0100

    initial scaffolding

---
+++ src/Servers/Server.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Kuroi\Cluster;
+
+class Server
+{
+    
+}
---
+++ tests/ManagerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kuroi\Cluster\Test;
+
+use Kuroi\Cluster\Manager;
+
+class ManagerTest extends PHPUnit_Framework_TestCase
+{
+
+}
