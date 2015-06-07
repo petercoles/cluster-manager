4b31166d03bebf77cab471afb50c4e3ea064b677
Peter Coles <peter@kuroiweb.com>
2015-06-07 23:19:01 +0100

    lightweight wrapper class for Guzzle

---
+++ src/HttpClients/GuzzleHttp.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Kuroi\Cluster\HttpClients;
+
+use Kuroi\Cluster\Contracts\HttpClientInterface;
+use GuzzleHttp\Client;
+
+class GuzzleHttp implements HttpClientInterface
+{
+    public $request;
+
+    /**
+     * Constructor - Instantiate Guzzle client using headers appropriate to adapter.
+     *
+     * @param integer $token
+     *
+     * @return GuzzleHttp\Client object
+     */
+    public function initClient($headers)
+    {
+        try {
+            $this->request = new Client($headers);
+        } catch (Exception $e) {
+            echo 'Unable to initialise http client because '. $e->getMessgae() . "\n";
+        }
+    }
+
+    /**
+     * Get status code from http response.
+     *
+     * @param GuzzleResponse $response
+     *
+     * @return integer
+     */
+    public function getStatus($response)
+    {
+        return (int) $response->getStatusCode();
+    }
+
+    /**
+     * Get http response body, cast to json and decode.
+     *
+     * @param GuzzleHttp\Response object $response
+     *
+     * @return array
+     */
+    public function getBody($response)
+    {
+        return json_decode((string) $response->getBody());
+    }
+}
