95277a9392a3736a8a07f6a3bac89ed56a5e8fbf
Peter Coles <peter@kuroiweb.com>
2015-06-07 01:15:39 +0100

    flesh out interface

--- src/Servers/Adapters/ServerAdapterInterface.php
+++ src/Servers/Adapters/ServerAdapterInterface.php
@@ -4,5 +4,9 @@ namespace Kuroi\Cluster\Servers\Adapters;
 
 interface ServerAdapterInterface
 {
+    public function read($id);
 
+    public function create($params);
+
+    public function delete($id);
 }
