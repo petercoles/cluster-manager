a0948459e7d0fd09c4ac31fb6d80c512f4387a7a
Peter Coles <peter@kuroiweb.com>
2015-06-07 23:17:03 +0100

    server class to pass requests to a chosen adapter, together with some initial, crude tests

--- src/Servers/Server.php
+++ src/Servers/Server.php
@@ -2,7 +2,71 @@
 
 namespace Kuroi\Cluster\Servers;
 
+use Kuroi\Cluster\Contracts\ServerAdapterInterface;
+use Kuroi\Cluster\Contracts\HttpClientInterface;
+
 class Server
 {
-    
+    protected $adapter;
+
+    /**
+     * Constructor - receive server adapter, http client and server authentication details.
+     * Initialise the http client and make it available for use by the server adapter.
+     *
+     * @param object $adapter
+     * @param object $client
+     * @param array $params
+     *
+     * @return null
+     */
+    public function __construct(ServerAdapterInterface $adapter, HttpClientInterface $client, array $authParams)
+    {
+        // set the server adapter
+        $this->adapter = $adapter;
+
+        // construct the authentication header options - dependent on the adapter being used
+        $headers = $this->adapter->requestHeaders($authParams);
+
+        // initialise the http client with the authentication header options
+        $client->initClient($headers);
+
+        // make the initialised http client available to the adapter
+        $this->adapter->setClient($client);
+    }
+
+    /**
+     * List details for an indexed server, or all servers if id is null.
+     *
+     * @param integer $id
+     *
+     * @return GuzzleResponse object | array of GuzzleResponse objects
+     */
+    public function read($id = null)
+    {
+        return $this->adapter->read($id);
+    }
+
+    /**
+     * Create a new server based on parameters received.
+     *
+     * @param array $params
+     *
+     * @return array
+     */
+    public function create($params)
+    {
+        return $this->adapter->create($params);
+    }
+
+    /**
+     * Delete the server corresponding to the given id.
+     *
+     * @param integer $id
+     *
+     * @return integer
+     */
+    public function delete($id)
+    {
+        return $this->adapter->delete($id);
+    }
 }
--- tests/ServerTest.php
+++ tests/ServerTest.php
@@ -2,12 +2,63 @@
 
 namespace Kuroi\Cluster\Test;
 
+use Mockery as m;
 use Kuroi\Cluster\Servers\Server;
 
 class ServerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testDummy()
+    public function setUp()
     {
+        $this->adapter = m::mock('\Kuroi\Cluster\Servers\Adapters\DigitalOcean');
+        $this->adapter
+            ->shouldReceive('requestHeaders')->once()->andReturn('headers')
+            ->shouldReceive('setClient')->once();
 
+        $this->client = m::mock('\Kuroi\Cluster\HttpClients\GuzzleHttp');
+        $this->client
+            ->shouldReceive('initClient')->once()->andReturn('client');
+
+        $this->server = new Server($this->adapter, $this->client, ['token' => 'foo']);
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testReadListOfDroplets()
+    {
+        $this->adapter->shouldReceive('read')->andReturn('bar');
+
+        $result = $this->server->read();
+
+        $this->assertEquals('bar', $result);
+    }
+
+    public function testReadDroplet()
+    {
+        $this->adapter->shouldReceive('read')->with(123)->andReturn('bar');
+
+        $result = $this->server->read(123);
+
+        $this->assertEquals('bar', $result);
+    }
+
+    public function testCreateDroplet()
+    {
+        $this->adapter->shouldReceive('create')->with(['image' => 'foo'])->andReturn('bar');
+
+        $result = $this->server->create(['image' => 'foo']);
+
+        $this->assertEquals('bar', $result);
+    }
+
+    public function testDeleteDroplet()
+    {
+        $this->adapter->shouldReceive('delete')->with(123)->andReturn(204);
+
+        $result = $this->server->delete(123);
+
+        $this->assertEquals(204, $result);
     }
 }
